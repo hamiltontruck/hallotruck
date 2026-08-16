1633f6d6bf06a4813988fdf9fe2c27f2739ad0e4
adilabdu52-coder <adilabdu52@gmail.com>
2026-08-16 04:49:17 +0300

    Add Sentry error monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import App from "./App";
 import "./index.css";
+
+Sentry.init({
+  dsn: "https://39d0747775820c372496eb775ff63b29@o4511917722894336.ingest.us.sentry.io/4511917875068928",
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
