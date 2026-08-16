c4e9f60d5c324b6ac492d4d67351447c4e9dde1a
hamiltontruck <hamiltoninternational8@gmail.com>
2026-08-16 21:31:57 +0300

    Scope fleet maintenance mobile styles

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import { CustomerPortal } from "./pages/CustomerPortal";
 import { DriverGate } from "./components/auth/DriverGate";
 import { Login } from "./pages/Login";
 import { LanguageProvider } from "./i18n/LanguageProvider";
+import "./styles/fleet-maintenance-mobile.css";
 
 function DriverShell({ children }: { children: React.ReactNode }) {
   return (
@@ -59,7 +60,7 @@ export default function App() {
           <Route path="/admin" element={<AdminGate><AdminWorkspace /></AdminGate>} />
           <Route path="/admin/driver-compliance" element={<AdminGate><AdminToolShell><AdminDriverCompliance /></AdminToolShell></AdminGate>} />
           <Route path="/admin/driver-commission" element={<AdminGate><AdminToolShell><AdminDriverCommission /></AdminToolShell></AdminGate>} />
-          <Route path="/admin/fleet-maintenance" element={<AdminGate><AdminToolShell><AdminFleetMaintenance /></AdminToolShell></AdminGate>} />
+          <Route path="/admin/fleet-maintenance" element={<AdminGate><AdminToolShell><div className="fleet-maintenance-mobile"><AdminFleetMaintenance /></div></AdminToolShell></AdminGate>} />
           <Route path="/customer/login" element={<CustomerLogin />} />
           <Route path="/customer" element={<CustomerGate><CustomerWorkspace /></CustomerGate>} />
           <Route path="/driver/login" element={<Login />} />
