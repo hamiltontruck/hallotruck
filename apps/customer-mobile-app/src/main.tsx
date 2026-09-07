84f9b79e3aa608f241a9f572e7acff57db618322
hamiltontruck <hamiltoninternational8@gmail.com>
2026-09-07 05:26:14 +0300

    Wire Customer mobile create-account auth boundary

--- apps/customer-mobile-app/src/main.tsx
+++ apps/customer-mobile-app/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { CustomerAuthBoundary } from "./auth/CustomerAuthBoundary";
+import { CustomerAuthBoundary } from "./auth/CustomerAuthBoundaryV2";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
