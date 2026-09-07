892ce1f99487e10cae96b83ba9c3020546925bc5
hamiltontruck <hamiltoninternational8@gmail.com>
2026-09-07 06:50:43 +0300

    Load Customer auth brand mark styles

--- apps/customer-mobile-app/src/main.tsx
+++ apps/customer-mobile-app/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { CustomerAuthBoundary } from "./auth/CustomerAuthBoundaryV2";
 import "./styles.css";
+import "./auth-brand.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
