395a8a68198ce037c1b472e77d5b219fbdb454c5
hamiltontruck <hamiltoninternational8@gmail.com>
2026-09-08 03:18:04 +0300

    Update Customer Android Native V4

---
+++ mobile/customer/android/app/build/generated/source/buildConfig/debug/com/hallo/logistics/customer/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.hallo.logistics.customer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.hallo.logistics.customer";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1.0";
+  // Field from default config.
+  public static final String MAPTILER_KEY = "5ZAhzwwpy8NeGYqTMkF1";
+  // Field from default config.
+  public static final String SUPABASE_PUBLISHABLE_KEY = "sb_publishable_GOdPb0EWJOz89FjPic5HIg_HdW5zCNU";
+  // Field from default config.
+  public static final String SUPABASE_URL = "https://febgayjolfrooaqenlje.supabase.co";
+}
